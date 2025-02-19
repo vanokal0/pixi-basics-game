97853ce72b6d69255def123758c54c63c791c68a
vanokal0 <vanokal0@users.noreply.github.com>
2025-02-19 20:48:57 +0200

    Menu initialization placed into MenuContainer class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { Application, Text, TextStyle } from "pixi.js";
+import { Application } from "pixi.js";
+import { MenuContainer } from "./scenes/MenuContainer";
 
 (async () => {
+  //application initialization
   const app: Application = new Application();
   await app.init({
     resizeTo: window,
@@ -9,20 +11,13 @@ import { Application, Text, TextStyle } from "pixi.js";
   app.canvas.style.position = "absolute";
   document.body.appendChild(app.canvas);
 
-  //text
-  const style: TextStyle = new TextStyle({
-    dropShadow: true,
-    fill: "#ffffff",
-    fontFamily: "Courier New",
-    fontSize: 42,
-    fontVariant: "small-caps",
-    fontWeight: "bolder",
-    padding: 30,
+  //menu scene
+  const menuContainer: MenuContainer = new MenuContainer();
+  menuContainer.initialize(app);
+
+  menuContainer.addEventListener("click", () => {
+    console.log("Hello World!");
   });
-  const startGameText: Text = new Text({ text: "Start Game", style: style });
-  startGameText.x = app.canvas.width / 2 - startGameText.width / 2;
-  startGameText.y = app.canvas.height / 2 - startGameText.height / 2;
-  startGameText.eventMode = "static";
-  startGameText.cursor = "poiner";
-  app.stage.addChild(startGameText);
+
+  app.stage.addChild(menuContainer);
 })();
